768a8ca5e468267712be5d8a7e91eb4e7690dbe9
Matt Graham <mgraham@yahoo-inc.com>
2019-03-26 13:54:36 -0500

    Update: Reports Ember 3.9

--- packages/reports/addon/components/filter-builders/base.js
+++ packages/reports/addon/components/filter-builders/base.js
@@ -1,16 +1,17 @@
 /**
- * Copyright 2018, Yahoo Holdings Inc.
+ * Copyright 2019, Yahoo Holdings Inc.
  * Licensed under the terms of the MIT license. See accompanying LICENSE.md file for terms.
  *
  * Base class for filter builders.
  */
-import Ember from 'ember';
+
+import { assign } from '@ember/polyfills';
+import Component from '@ember/component';
+import { get, computed } from '@ember/object';
 import layout from 'navi-reports/templates/components/filter-builders/base';
 import { readOnly } from '@ember/object/computed';
 
-const { get } = Ember;
-
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
 
   /**
@@ -37,7 +38,9 @@ export default Ember.Component.extend({
   /**
    * @property {Array} supportedOperators - list of valid values for filter.operator
    */
-  supportedOperators: [],
+  supportedOperators: computed(function() {
+    return [];
+  }),
 
   actions: {
     /**
@@ -52,7 +55,7 @@ export default Ember.Component.extend({
        * unless operators share valuesComponent
        */
       if (get(this, 'filter.operator.valuesComponent') !== operatorObject.valuesComponent) {
-        Ember.assign(changeSet, { values: [] });
+        assign(changeSet, { values: [] });
       }
 
       this.onUpdateFilter(changeSet);
